e7b15fbb84d4b12ec4f5951b6a8c49a470be1f3a
oneliang <oneliang@tencent.com>
2017-03-27 19:38:38 +0800

    optimize code

--- src/main/java/com/oneliang/tools/builder/android/handler/MergeAndroidManifestHandler.java
+++ src/main/java/com/oneliang/tools/builder/android/handler/MergeAndroidManifestHandler.java
@@ -1,5 +1,6 @@
 package com.oneliang.tools.builder.android.handler;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,18 +10,31 @@ import com.oneliang.util.file.FileUtil;
 
 public class MergeAndroidManifestHandler extends AbstractAndroidHandler {
 
-	public boolean handle() {
-		List<String> libraryAndroidManifestList=new ArrayList<String>();
-		for(AndroidProject androidProject:this.androidConfiguration.getAndroidProjectList()){
-//			if(!this.androidConfiguration.getProjectMain().equals(androidProject.getName())){
-				libraryAndroidManifestList.addAll(androidProject.getAndroidManifestList());
-//			}
-		}
-		String mainProjectAndroidManifest=libraryAndroidManifestList.remove(0);//this.androidConfiguration.getMainAndroidProject().getAndroidManifest();
-//		logger.log("main android manifest:"+mainProjectAndroidManifest+",library android manifest size:"+libraryAndroidManifestList.size());
-		String androidManifestOutput=this.androidConfiguration.getPublicAndroidProject().getAndroidManifestOutput();
-		FileUtil.createFile(androidManifestOutput);
-		BuilderUtil.mergeAndroidManifest(mainProjectAndroidManifest, libraryAndroidManifestList, androidManifestOutput,true);//this.androidConfiguration.isApkDebug());
-		return true;
-	}
+    public boolean handle() {
+        List<String> allAndroidManifestList = new ArrayList<String>();
+        for (AndroidProject androidProject : this.androidConfiguration.getAndroidProjectList()) {
+            // if(!this.androidConfiguration.getProjectMain().equals(androidProject.getName())){
+            allAndroidManifestList.addAll(androidProject.getAndroidManifestList());
+            // }
+            if (androidProject.getAndroidManifestList() != null) {
+                for (String androidManifest : androidProject.getAndroidManifestList()) {
+                    logger.info(androidManifest);
+                }
+            }
+            for (String sourceDirectory : androidProject.getSourceDirectoryList()) {
+                File sourceDirectoryFile = new File(sourceDirectory);
+                if (sourceDirectoryFile.getName().equals("src")) {
+                    String androidManifest = new File(sourceDirectoryFile.getParent(), "AndroidManifest.xml").getAbsolutePath();
+                    if (FileUtil.isExist(androidManifest)) {
+                        allAndroidManifestList.add(androidManifest);
+                    }
+                }
+            }
+        }
+        logger.info("all android manifest size:" + allAndroidManifestList.size());
+        String androidManifestOutput = this.androidConfiguration.getPublicAndroidProject().getAndroidManifestOutput();
+        FileUtil.createFile(androidManifestOutput);
+        BuilderUtil.mergeAndroidManifest(this.androidConfiguration.getPackageName(), allAndroidManifestList, androidManifestOutput, true);// this.androidConfiguration.isApkDebug());
+        return true;
+    }
 }
