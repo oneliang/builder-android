c88763bcca359e28e1d3a29032a61612830fcc61
oneliang <oneliang@tencent.com>
2017-04-21 16:47:07 +0800

    debug or not judge by configuration

--- src/main/java/com/oneliang/tools/builder/android/handler/MergeAndroidManifestHandler.java
+++ src/main/java/com/oneliang/tools/builder/android/handler/MergeAndroidManifestHandler.java
@@ -37,7 +37,7 @@ public class MergeAndroidManifestHandler extends AbstractAndroidHandler {
         logger.info("all android manifest size:" + allAndroidManifestList.size());
         String androidManifestOutput = this.androidConfiguration.getPublicAndroidProject().getAndroidManifestOutput();
         FileUtil.createFile(androidManifestOutput);
-        BuilderUtil.mergeAndroidManifest(this.androidConfiguration.getPackageName(), this.androidConfiguration.getMinSdkVersion(), this.androidConfiguration.getTargetSdkVersion(), allAndroidManifestList, androidManifestOutput, true);// this.androidConfiguration.isApkDebug());
+        BuilderUtil.mergeAndroidManifest(this.androidConfiguration.getPackageName(), this.androidConfiguration.getMinSdkVersion(), this.androidConfiguration.getTargetSdkVersion(), allAndroidManifestList, androidManifestOutput, this.androidConfiguration.isApkDebug());
         return true;
     }
 }
